619b224f869fffccc7cd6c1c8a0cd40a874011db
YentlWils <yentl_wils@hotmail.com>
2015-10-23 14:24:52 +0200

    Change social media links

--- dev/js/app.services.js
+++ dev/js/app.services.js
@@ -2,13 +2,20 @@
     var app = angular.module('skdebrug.services', []);
 
     var api = {
-        players: 'data/player.json',
-        news: 'data/news.json',
-        newsItem: 'data/news-item.json?id={0}',
-        nextMatch: 'data/next-match.json',
-        allMatches: 'data/all-matches.json',
-        prevMatch: 'data/prev-match.json',
-        league: 'data/league.json',
+        //players: 'data/player.json',
+        //news: 'data/news.json',
+        //newsItem: 'data/news-item.json?id={0}',
+        //nextMatch: 'data/next-match.json',
+        //allMatches: 'data/all-matches.json',
+        //prevMatch: 'data/prev-match.json',
+        //league: 'data/league.json',
+        players: '/data/player',
+        news: '/data/news',
+        newsItem: '/data/news/{0}',
+        nextMatch: '/data/game/next/{0}',
+        allMatches: '/data/game',
+        prevMatch: '/data/game/prev/{0}',
+        league: '/data/league',
     };
 
     app.factory('apiServices', function ($http) {
